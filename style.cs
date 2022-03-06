d2b8452e0617802e56562245960d44f82915f9b9
Jideotetic <53506936+Jideotetic@users.noreply.github.com>
2022-03-06 15:40:21 +0100

    Create style.css

---
+++ style.cs
@@ -0,0 +1,119 @@
+* {
+     box-sizing: box-border;
+ }
+body {
+    margin: 0;
+    background-color: black;
+}
+
+header {
+    border:1px solid white;
+    height: 150px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px white;
+    width: 100%;
+}
+
+.resume-title {
+    border:1px solid pink;
+    height: 50px;
+    margin: 10px auto 10px auto;
+    box-shadow:2px 2px pink;
+    width: 50%;
+}
+
+.contact-address {
+    border:1px solid pink;
+    height: 50px;
+    margin: 10px auto 10px auto;
+    box-shadow:2px 2px pink;
+    width: 50%;
+}
+
+main {
+    border:1px solid white;
+    height: 500px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px white;
+    width: 100%;
+}
+
+.profile {
+    border:1px solid pink;
+    height: 50px;
+    margin: 10px auto 10px auto;
+    box-shadow:2px 2px pink;
+    width: 50%;
+}
+
+.experience {
+    border:1px solid pink;
+    height: 150px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px pink;
+    width: 90%;
+}
+
+.algorithm {
+    border:1px solid pink;
+    height: 100px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px pink;
+    width: 40%;
+}
+
+.ecart {
+    border:1px solid pink;
+    height: 100px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px pink;
+    width: 40%;
+}
+
+.experience-float {
+    float: left;
+}
+
+.education {
+    border:1px solid pink;
+    height: 50px;
+    margin: 10px auto 10px auto;
+    box-shadow:2px 2px pink;
+    width: 50%;
+}
+
+.cert-skills {
+    border:1px solid pink;
+    height: 150px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px pink;
+    width: 90%;
+}
+
+.certifications {
+    border:1px solid pink;
+    height: 100px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px pink;
+    width: 40%;
+}
+
+.skills {
+    border:1px solid pink;
+    height: 100px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px pink;
+    width: 40%;
+}
+
+.cert-skills-float {
+    float: left;
+}
+
+footer {
+    border:1px solid white;
+    height: 50px;
+    margin: 10px 10px 10px 10px;
+    box-shadow:2px 2px white;
+    width: 100%;
+}
