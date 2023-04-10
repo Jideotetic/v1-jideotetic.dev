8573c8ff956bd34e6888c320d54122e5587803fb
Jideotetic <jideotetic@gmail.com>
2023-04-11 00:37:20 +0100

    Updated the design

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+const contentContainer = document.getElementById("content-container");
+const loadingSpinner = document.getElementById("loading-spinner");
+const navBar = document.getElementById("nav-bar");
+const navLinks = navBar.getElementsByClassName("nav-link");
+const toggle = document.getElementById("toggle");
+
+// handleLoading() is called when the page is loaded
 function handleLoading() {
-  document.getElementById("content-container").style.display = "block";
-  document.getElementById("loading-spinner-container").style.display = "none";
+  contentContainer.style.display = "block";
+  loadingSpinner.style.display = "none";
 }
+
+// Collapse the navbar when a link is clicked
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    if (toggle.checked) {
+      navBar.style.maxHeight = "0px";
+      toggle.checked = false;
+    }
+  });
+}
+
+// Expand and collapse the navbar when the menu icon is clicked
+toggle.addEventListener("click", function () {
+  if (toggle.checked) {
+    navBar.style.maxHeight = "100%";
+  } else {
+    navBar.style.maxHeight = "0px";
+  }
+});
