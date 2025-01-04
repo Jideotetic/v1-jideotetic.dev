77228c8316fe2ccb3e98e25e4df2cffc3db9db3a
Jideotetic <jideotetic@gmail.com>
2025-01-04 15:00:49 +0100

    Redesign hero

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router";
 import type { Route } from "./+types/home";
 import logo from "./JD.svg";
+import illustration from "./3d-illustration.png";
+
 import {
 	Popover,
 	PopoverBackdrop,
@@ -9,9 +11,10 @@ import {
 } from "@headlessui/react";
 import Nav from "~/components/nav";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export function meta({}: Route.MetaArgs) {
-	return [{ title: "Abdulbasit Yusuf | Software Engineer" }];
+	return [{ title: "Abdulbasit Yusuf | Frontend Developer" }];
 }
 
 export default function Home() {
@@ -73,101 +76,35 @@ export default function Home() {
 				</div>
 			</header>
 
-			<section className="pt-[112px] bg-gradient-to-b from-gray-50 via-white to-gray-50 border-2 border-red-500">
+			<section className="pt-[112px] bg-gradient-to-b from-gray-50 via-white to-gray-50">
 				<div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-					<div className="grid max-w-md grid-cols-1 mx-auto lg:grid-cols-12 gap-x-6 gap-y-8 lg:max-w-none">
-						<div className="self-center lg:col-span-4">
+					<div className="grid max-w-md grid-cols-1 mx-auto md:grid-cols-12 gap-x-6 gap-y-8 md:max-w-none">
+						<div className="self-center md:col-span-6">
 							<h1 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl">
-								Hey 👋 I am Brian Jones, writing on NFTs.
+								Hey 👋 I am Abdulbasit Yusuf, Frontend Developer!
 							</h1>
 							<p className="mt-5 text-base font-normal leading-7 text-gray-500">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-								Vehicula massa in enim luctus.
+								A new day...another opportunity to become world class
 							</p>
 
-							<div className="relative inline-flex mt-9 group">
-								<div className="absolute transitiona-all duration-1000 opacity-70 inset-0 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"></div>
+							<div className="relative inline-flex mt-9 space-x-2 text-xl">
+								<Link to="https://github.com/Jideotetic" title="Github Profile">
+									<FaGithub />
+								</Link>
 
-								<a
-									href="#"
-									title=""
-									className="relative inline-flex items-center justify-center px-8 py-3 sm:text-sm sm:py-3.5 text-base font-semibold text-white transition-all duration-200 bg-indigo-600 border border-transparent rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
-									role="button"
+								<Link
+									to="https://www.linkedin.com/in/jideotetic/"
+									title="LinkedIn Profile"
 								>
-									Read Exclusive Blog
-								</a>
+									<FaLinkedin />
+								</Link>
 							</div>
 						</div>
 
-						<div className="self-end lg:order-last lg:pb-20 lg:col-span-3">
-							<p className="text-xs font-bold tracking-widest text-gray-500 uppercase">
-								⚡️ Latest Picks
-							</p>
-
-							<div className="mt-6 space-y-6 lg:space-y-8">
-								<div className="relative overflow-hidden">
-									<div className="flex items-start lg:items-center">
-										<img
-											className="object-cover w-12 h-12 rounded-lg shrink-0"
-											src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/1/thumbnail-1.png"
-											alt=""
-										/>
-										<p className="ml-5 text-base font-bold leading-6 text-gray-900">
-											<a href="#" title="">
-												How a visual artist redefines success in graphic design
-												<span
-													className="absolute inset-0"
-													aria-hidden="true"
-												></span>
-											</a>
-										</p>
-									</div>
-								</div>
-							</div>
-
-							<div className="relative overflow-hidden">
-								<div className="flex items-start lg:items-center">
-									<img
-										className="object-cover w-12 h-12 rounded-lg shrink-0"
-										src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/1/thumbnail-2.png"
-										alt=""
-									/>
-									<p className="ml-5 text-base font-bold leading-6 text-gray-900">
-										<a href="#" title="">
-											How a visual artist redefines success in graphic design
-											<span
-												className="absolute inset-0"
-												aria-hidden="true"
-											></span>
-										</a>
-									</p>
-								</div>
-							</div>
-
-							<div className="relative overflow-hidden">
-								<div className="flex items-start lg:items-center">
-									<img
-										className="object-cover w-12 h-12 rounded-lg shrink-0"
-										src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/1/thumbnail-3.png"
-										alt=""
-									/>
-									<p className="ml-5 text-base font-bold leading-6 text-gray-900">
-										<a href="#" title="">
-											How a visual artist redefines success in graphic design
-											<span
-												className="absolute inset-0"
-												aria-hidden="true"
-											></span>
-										</a>
-									</p>
-								</div>
-							</div>
-						</div>
-
-						<div className="self-end lg:col-span-5">
+						<div className="self-end md:col-span-6">
 							<img
-								className="w-full mx-auto"
-								src="https://landingfoliocom.imgix.net/store/collection/clarity-blog/images/hero/1/author.png"
+								className="w-full max-w-xs mx-auto"
+								src={illustration}
 								alt=""
 							/>
 						</div>
